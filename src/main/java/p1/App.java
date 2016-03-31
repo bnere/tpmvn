5ba40752fd984b7c4176b6542a3fe8bec6e3207c
dudcl01 <dudcl01@148.60.14.118>
2016-03-31 08:38:23 +0200

    initial

---
+++ src/main/java/p1/App.java
@@ -0,0 +1,14 @@
+package p1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	String codeMort; // test maven-pmd-plugin
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
